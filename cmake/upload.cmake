3094adc8acae27894e7c24b61cbdb0dd058a5c09
Michael Hirsch <scivision@users.noreply.github.com>
2021-06-07 14:01:06 -0400

    upload: also update ref_data.json

--- cmake/upload.cmake
+++ cmake/upload.cmake
@@ -4,9 +4,11 @@ function(upload_package archive out_dir name upload_root)
 # NOTE: rclone copy default does not overwrite
 
 # upload archive itself
-execute_process(COMMAND rclone copy ${archive} ${PACKAGE_REMOTE}:${upload_root} --verbose
-TIMEOUT 1800
-COMMAND_ERROR_IS_FATAL ANY)
+execute_process(
+  COMMAND rclone copy ${archive} ${PACKAGE_REMOTE}:${upload_root} --verbose
+  COMMAND rclone copy ${ref_json_file} ${PACKAGE_REMOTE}:${upload_root} --verbose
+  TIMEOUT 1800
+  COMMAND_ERROR_IS_FATAL ANY)
 
 # upload plots directory to avoid needing to extract on local computers
 # that is for others to quickly preview plots

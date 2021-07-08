6609f890e8d4ef2ebfa66953e8d24a5d042ecdb2
Michael Hirsch <scivision@users.noreply.github.com>
2021-07-08 07:46:22 -0400

    ci: prepare for CMake 3.22, remove unused code

--- ci.cmake
+++ ci.cmake
@@ -48,7 +48,7 @@ if(CTEST_MODEL STREQUAL Nightly OR CTEST_MODEL STREQUAL Continuous)
   endif()
 endif()
 
-if(NOT CMAKE_BUILD_TYPE)
+if(NOT CMAKE_BUILD_TYPE AND NOT DEFINED ENV{CMAKE_BUILD_TYPE})
   # RelWithDebInfo -O2, Release -O3
   set(CMAKE_BUILD_TYPE Release)
 endif()
@@ -74,7 +74,7 @@ if(NOT DEFINED CTEST_BUILD_NAME)
       NAMES gemini3d.run
       HINTS ${GEMINI_ROOT} ENV GEMINI_ROOT
       PATHS ${CTEST_SOURCE_DIRECTORY}/../gemini3d
-      PATH_SUFFIXES build bin
+      PATH_SUFFIXES build bin build/Release build/RelWithDebInfo build/Debug
       DOC "Gemini3d.run Fortran front-end"
       REQUIRED)
 
@@ -132,29 +132,7 @@ if(NOT DEFINED CTEST_CMAKE_GENERATOR)
 endif()
 
 # --- test parallelism
-include(ProcessorCount)
-
-function(cmake_cpu_count)
-  # on ARM e.g. Raspberry Pi, the usually reliable cmake_host_system_info gives 1 instead of true count
-  # fallback to less reliable ProcessorCount which does work on Raspberry Pi.
-
-cmake_host_system_information(RESULT sys_info QUERY OS_NAME OS_PLATFORM)
-if(sys_info STREQUAL "macOS;arm64")
-  # Apple Silicon M1 workaround for hwloc et al:
-  # https://github.com/open-mpi/hwloc/issues/454
-  cmake_host_system_information(RESULT Nhybrid QUERY NUMBER_OF_PHYSICAL_CORES)
-
-  math(EXPR Ncpu "${Nhybrid} / 2")  # use only fast cores, else MPI very slow
-
-  message(STATUS "Apple M1 hybrid CPU count workaround applied.")
-else()
-  ProcessorCount(_ncount)
-  cmake_host_system_information(RESULT Ncpu QUERY NUMBER_OF_PHYSICAL_CORES)
-
-  if(Ncpu EQUAL 1 AND _ncount GREATER 0)
-    set(Ncpu ${_ncount})
-  endif()
-endif()
+cmake_host_system_information(RESULT Ncpu QUERY NUMBER_OF_PHYSICAL_CORES)
 
 # limit RAM use as Matlab/Python in parallel can use a lot of RAM with 3D grids
 cmake_host_system_information(RESULT ram QUERY TOTAL_PHYSICAL_MEMORY)
@@ -168,11 +146,7 @@ if(ram LESS 18000)
 endif()
 list(APPEND opts -Dlow_ram:BOOL=${low_ram})
 
-set(Ncpu ${Ncpu} PARENT_SCOPE)
 
-endfunction(cmake_cpu_count)
-
-cmake_cpu_count()
 message(STATUS "using Ncpu = ${Ncpu}")
 
 # --- CTest Dashboard

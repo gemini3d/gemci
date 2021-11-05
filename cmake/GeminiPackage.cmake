78d5120e43aa1476ca6125b896256e88b49c00bb
Michael Hirsch <scivision@users.noreply.github.com>
2021-11-04 21:28:17 -0400

    dedupe name/label

--- cmake/GeminiPackage.cmake
+++ cmake/GeminiPackage.cmake
@@ -7,11 +7,11 @@ function(gemini_package GEMINI_CIROOT out_dir ref_json_file name label)
 cmake_path(APPEND archive ${upload_root} ${name}.${ARC_TYPE})
 cmake_path(APPEND data_dir ${GEMINI_CIROOT} ${name})
 
-add_test(NAME "package:archive:${name}"
+add_test(NAME "archive:${name}"
 COMMAND ${CMAKE_COMMAND} -Din:PATH=${data_dir} -Dout:FILEPATH=${archive} -Dref_json_file:FILEPATH=${ref_json_file} -Dgemini_version=${GEMINI_VERSION} -Dgemini_features=${GEMINI_FEATURES} -Dpygemini_version=${PYGEMINI_VERSION} -Dname=${name} -P ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/archive.cmake
 )
 
-set_tests_properties("package:archive:${name}" PROPERTIES
+set_tests_properties("archive:${name}" PROPERTIES
 FIXTURES_REQUIRED "${name}:run_fxt;${name}:plot_fxt"
 FIXTURES_SETUP ${name}:upload_fxt
 DISABLED ${${name}_DISABLED}
@@ -23,11 +23,11 @@ TIMEOUT 120
 
 find_program(rclone NAMES rclone)
 
-add_test(NAME "package:upload:${name}"
+add_test(NAME "upload:${name}"
 COMMAND ${CMAKE_COMMAND} -Darchive:FILEPATH=${archive} -Dout_dir:PATH=${out_dir} -Dref_json_file:FILEPATH=${ref_json_file} -Dname=${name} -Dupload_root:PATH=gemini_upload-${package_date} -P ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/upload.cmake
 )
 
-set_tests_properties("package:upload:${name}" PROPERTIES
+set_tests_properties("upload:${name}" PROPERTIES
 FIXTURES_REQUIRED ${name}:upload_fxt
 LABELS "package;${label}"
 REQUIRED_FILES ${archive}

0842c8c1c0a3ba4bd611dd45fad82bb2641e66de
Michael Hirsch <scivision@users.noreply.github.com>
2021-11-22 10:46:22 -0500

    autobuild gemini3d: debug and release

--- cmake/build_gemini3d.cmake
+++ cmake/build_gemini3d.cmake
@@ -1,29 +1,41 @@
+# in general, we need both Release and Debug of Gemini3D.
+# to ensure this happens without requiring Ninja Multi-Config, we do two distinct builds
+# with the same install directory.
 include(ExternalProject)
+
+if(NOT GEMINI_ROOT)
+  set(GEMINI_ROOT ${CMAKE_INSTALL_PREFIX})
+endif()
 
 set(gemini_args
 -DBUILD_TESTING:BOOL=off
 -Dmsis2:BOOL=on
---install-prefix=${PROJECT_BINARY_DIR}
--DCMAKE_BUILD_TYPE=Debug
+--install-prefix=${GEMINI_ROOT}
 )
 
 file(READ ${PROJECT_SOURCE_DIR}/libraries.json _libj)
 string(JSON gemini_url GET ${_libj} gemini3d url)
 string(JSON gemini_tag GET ${_libj} gemini3d tag)
 
-ExternalProject_Add(GEMINI3D
+ExternalProject_Add(GEMINI3D_RELEASE
 GIT_REPOSITORY ${gemini_url}
 GIT_TAG ${gemini_tag}
-CMAKE_ARGS ${gemini_args}
+CMAKE_ARGS ${gemini_args} -DCMAKE_BUILD_TYPE=Release
 CMAKE_GENERATOR ${EXTPROJ_GENERATOR}
 INACTIVITY_TIMEOUT 15
 CONFIGURE_HANDLED_BY_BUILD true
 )
 
-ExternalProject_Get_property(GEMINI3D SOURCE_DIR)
-cmake_path(NORMAL_PATH SOURCE_DIR OUTPUT_VARIABLE GEMINI_ROOT)
+ExternalProject_Add(GEMINI3D_DEBUG
+GIT_REPOSITORY ${gemini_url}
+GIT_TAG ${gemini_tag}
+CMAKE_ARGS ${gemini_args} -DCMAKE_BUILD_TYPE=Debug
+CMAKE_GENERATOR ${EXTPROJ_GENERATOR}
+INACTIVITY_TIMEOUT 15
+CONFIGURE_HANDLED_BY_BUILD true
+)
 
-set(GEMINI_RUN_DEBUG ${PROJECT_BINARY_DIR}/bin/gemini3d.run.debug)
-set(GEMINI_RUN ${PROJECT_BINARY_DIR}/bin/gemini3d.run)
-set(GEMINI_COMPARE ${PROJECT_BINARY_DIR}/bin/gemini3d.compare)
+set(GEMINI_RUN_DEBUG ${GEMINI_ROOT}/bin/gemini3d.run.debug)
+set(GEMINI_RUN ${GEMINI_ROOT}/bin/gemini3d.run)
+set(GEMINI_COMPARE ${GEMINI_ROOT}/bin/gemini3d.compare)
 set(GEMINI_RUN_BOUNDS_CHECK true CACHE BOOL "assume flags OK on self-build")
--- cmake/options.cmake
+++ cmake/options.cmake
@@ -29,6 +29,13 @@ else()
   set(FETCHCONTENT_UPDATES_DISCONNECTED_MATGEMINI true)
 endif()
 
+# --- default install directory under build/local
+# users can specify like "cmake -B build -DCMAKE_INSTALL_PREFIX=~/mydir"
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+  # will not take effect without FORCE
+  set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR} CACHE PATH "Install top-level directory" FORCE)
+endif()
+
 # --- auto-ignore build directory
 if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
   file(WRITE ${PROJECT_BINARY_DIR}/.gitignore "*")

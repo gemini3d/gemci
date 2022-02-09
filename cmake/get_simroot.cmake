5d16c0179014a566be2cf1e481fb9a040f43c80f
Michael Hirsch <scivision@users.noreply.github.com>
2022-02-09 14:54:33 -0500

    GEMINI_CIROOT: default to build dir

--- cmake/get_simroot.cmake
+++ cmake/get_simroot.cmake
@@ -1,16 +1,19 @@
 # the directory where all simulation output dirs are under.
 # Since we use one sim to drive another, and we don't want to
-# erase long runs
+# erase long runs.
+# By default the data directory is under the build directory, as would
+# be expected to avoid data state in a CI system.
+# Developers may choose to use a directory outside the build directory
+# to avoid redownloading data when intensively developing GemCI.
+# The latter behavior runs the risk of data state where a bug isn't
+# realized due to something in locally cached data.
 if(NOT GEMINI_CIROOT AND DEFINED ENV{GEMINI_CIROOT})
-  set(GEMINI_CIROOT $ENV{GEMINI_CIROOT})
+  cmake_path(SET GEMINI_CIROOT $ENV{GEMINI_CIROOT})
 endif()
 
 if(NOT GEMINI_CIROOT)
-  message(FATAL_ERROR "please specify GEMINI_CIROOT, either by:
-
-  * set environment variable GEMINI_CIROOT
-  * at configure time:  cmake -DGEMINI_CIROOT:PATH=/path/to/CI-data
-  ")
+  cmake_path(SET GEMINI_CIROOT ${CMAKE_CURRENT_BINARY_DIR})
+  message(STATUS "Set GEMINI_CIROOT ${GEMINI_CIROOT}")
 endif()
 
 if(CMAKE_VERSION VERSION_LESS 3.21)
@@ -18,13 +21,6 @@ if(CMAKE_VERSION VERSION_LESS 3.21)
 else()
   file(REAL_PATH ${GEMINI_CIROOT} GEMINI_CIROOT EXPAND_TILDE)
 endif()
-cmake_path(APPEND ref_root ${GEMINI_CIROOT} test_ref)
+cmake_path(SET ref_root ${GEMINI_CIROOT}/test_ref)
 
-if(NOT IS_DIRECTORY ${GEMINI_CIROOT})
-  file(MAKE_DIRECTORY ${GEMINI_CIROOT})
-endif()
-
-if(NOT IS_DIRECTORY ${ref_root})
-  message("Creating reference data directory ${ref_root}")
-  file(MAKE_DIRECTORY ${ref_root})
-endif()
+file(MAKE_DIRECTORY ${ref_root})

1535571ee95afb6a964c7e5957d539555fd3329f
Michael Hirsch <scivision@users.noreply.github.com>
2022-04-26 15:38:58 -0400

    ci.cmake: simplify [skip ci]

--- ci.cmake
+++ ci.cmake
@@ -16,13 +16,11 @@ if(DEFINED ENV{CI})
   set(CI $ENV{CI})
 endif()
 
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 set(CTEST_SUBMIT_URL "https://my.cdash.org/submit.php?project=${CTEST_PROJECT_NAME}")
 
 # --- Experimental, Nightly, Continuous
 # https://cmake.org/cmake/help/latest/manual/ctest.1.html#dashboard-client-modes
-if(NOT CTEST_MODEL AND DEFINED ENV{CTEST_MODEL})
-  set(CTEST_MODEL $ENV{CTEST_MODEL})
-endif()
 if(NOT CTEST_MODEL AND CI)
   set(CTEST_MODEL "Nightly")
 endif()
@@ -55,27 +53,6 @@ endif()
 
 find_program(GIT_EXECUTABLE NAMES git REQUIRED)
 
-if(NOT DEFINED CTEST_BUILD_NAME)
-  if(DEFINED ENV{CTEST_BUILD_NAME})
-    set(CTEST_BUILD_NAME $ENV{CTEST_BUILD_NAME})
-  else()
-    # a priori we are going to use the latest Git commit
-    execute_process(COMMAND ${GIT_EXECUTABLE} ls-remote
-      https://github.com/gemini3d/gemini3d.git
-      fclaw_prep3
-    OUTPUT_VARIABLE raw OUTPUT_STRIP_TRAILING_WHITESPACE
-    RESULT_VARIABLE err
-    TIMEOUT 15
-    )
-
-    if(err EQUAL 0)
-      string(REGEX MATCH "([a-f]|[0-9])+" gemini_git_version ${raw})
-      set(CTEST_BUILD_NAME ${gemini_git_version})
-    endif()
-  endif()
-endif()
-
-
 # --- find generator
 function(find_generator)
 
@@ -91,24 +68,21 @@ find_program(ninja NAMES ninja ninja-build samu)
 if(ninja)
   execute_process(COMMAND ${ninja} --version
   OUTPUT_VARIABLE ninja_version OUTPUT_STRIP_TRAILING_WHITESPACE
-  RESULT_VARIABLE err
+  RESULT_VARIABLE ret
   TIMEOUT 5
   )
-  if(err EQUAL 0 AND ninja_version VERSION_GREATER_EQUAL 1.10)
-    set(CTEST_CMAKE_GENERATOR Ninja)
+  if(ret EQUAL 0 AND ninja_version VERSION_GREATER_EQUAL 1.10)
+    set(CTEST_CMAKE_GENERATOR Ninja PARENT_SCOPE)
+    return()
   endif()
 endif(ninja)
 
-if(NOT CTEST_CMAKE_GENERATOR)
-  set(CTEST_BUILD_FLAGS -j)  # not --parallel as this goes to generator directly
-  if(WIN32)
-    set(CTEST_CMAKE_GENERATOR "MinGW Makefiles")
-  else()
-    set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-  endif()
+set(CTEST_BUILD_FLAGS -j)  # not --parallel as this goes to generator directly
+if(WIN32)
+  set(CTEST_CMAKE_GENERATOR "MinGW Makefiles" PARENT_SCOPE)
+else()
+  set(CTEST_CMAKE_GENERATOR "Unix Makefiles" PARENT_SCOPE)
 endif()
-
-set(CTEST_CMAKE_GENERATOR ${CTEST_CMAKE_GENERATOR} PARENT_SCOPE)
 
 endfunction(find_generator)
 
@@ -120,7 +94,6 @@ set(CTEST_SUBMIT_RETRY_COUNT 2)
 # avoid auto-detect version control failures on some systems
 set(CTEST_UPDATE_TYPE git)
 set(CTEST_UPDATE_COMMAND git)
-set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 ctest_start(${CTEST_MODEL})
 
@@ -134,14 +107,14 @@ if(CTEST_MODEL MATCHES "(Nightly|Continuous)")
   COMMAND_ERROR_IS_FATAL ANY
   )
   if(ret)
-    message(FATAL_ERROR "CTest would have erased the non-Git Push'd changes, NOT updating.")
+    message(FATAL_ERROR "CTest would have erased the non-Git Push'd changes.")
   else()
     ctest_update(
     RETURN_VALUE ret
     CAPTURE_CMAKE_ERROR err
     )
     if(ret LESS 0 OR NOT err EQUAL 0)
-      message(FATAL_ERROR "Configure ${build_id} failed: return ${ret} cmake return ${err}")
+      message(FATAL_ERROR "Update failed: return ${ret} cmake return ${err}")
     endif()
   endif()
 
@@ -164,6 +137,23 @@ if(CTEST_MODEL MATCHES "(Nightly|Continuous)")
 
   endif()
 endif()
+
+# --- CTEST_BUILD_NAME is used by ctest_submit()
+
+if(NOT DEFINED CTEST_BUILD_NAME)
+  # a priori we are going to use the latest Git commit
+  execute_process(COMMAND ${GIT_EXECUTABLE} ls-remote
+    https://github.com/gemini3d/gemini3d.git
+    fclaw_prep3
+  OUTPUT_VARIABLE raw OUTPUT_STRIP_TRAILING_WHITESPACE
+  TIMEOUT 15
+  COMMAND_ERROR_IS_FATAL ANY
+  )
+  string(REGEX MATCH "([a-f]|[0-9])+" gemini_git_version ${raw})
+  set(CTEST_BUILD_NAME ${gemini_git_version})
+endif()
+
+# --- configure
 
 ctest_configure(
 OPTIONS "${opts}"

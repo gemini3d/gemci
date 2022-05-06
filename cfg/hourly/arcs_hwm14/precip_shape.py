db4f892788d7cc5a0cdb4d1f706766d91caf3030
Michael Hirsch <scivision@users.noreply.github.com>
2022-05-06 13:02:23 -0400

    hwm14 cfg/ dirs

---
+++ cfg/hourly/arcs_hwm14/precip_shape.py
@@ -0,0 +1,33 @@
+import numpy as np
+import xarray
+
+
+def precip_shape(pg: xarray.Dataset, Qpeak: float, Qbackground: float) -> xarray.DataArray:
+    """
+    makes a 2D Gaussian shape in Latitude, Longitude
+    """
+
+    mlon_mean = pg.mlon.mean().item()
+    mlat_mean = pg.mlat.mean().item()
+
+    displace = 10 * pg.mlat_sigma
+
+    mlatctr = mlat_mean + displace * np.tanh((pg.mlon - mlon_mean) / (2 * pg.mlon_sigma))
+    # changed so the arc is wider compared to its twisting
+    Q = (
+        Qpeak
+        * np.exp(-((pg.mlon - mlon_mean) ** 2) / 2 / pg.mlon_sigma ** 2)
+        * np.exp(-((pg.mlat - mlatctr - 1.5 * pg.mlat_sigma) ** 2) / 2 / pg.mlat_sigma ** 2)
+    )
+
+    # from matplotlib.pyplot import figure,show
+    # fg = figure()
+    # ax = fg.gca()
+    # hi = ax.pcolormesh(pg.mlon, pg.mlat, Q)
+    # fg.colorbar(hi, ax=ax)
+    # ax.set_title("arcs: precip_shape: Q")
+    # ax.set_xlabel("MLON")
+    # ax.set_ylabel("MLAT")
+    # show()
+
+    return Q.clip(min=Qbackground)

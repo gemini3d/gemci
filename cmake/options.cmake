268260c5e43ceee4e33275bbfbce14918c004991
Michael Hirsch <scivision@users.noreply.github.com>
2022-11-02 01:09:36 -0400

    don't need to specify extproj generator

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -24,13 +24,6 @@ else()
   set_directory_properties(PROPERTIES EP_UPDATE_DISCONNECTED false)
 endif()
 
-# --- for ExternalProject generator
-if(CMAKE_GENERATOR STREQUAL "Ninja Multi-Config")
-  set(EXTPROJ_GENERATOR "Ninja")
-else()
-  set(EXTPROJ_GENERATOR ${CMAKE_GENERATOR})
-endif()
-
 # --- auto-ignore build directory
 if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
   file(WRITE ${PROJECT_BINARY_DIR}/.gitignore "*")

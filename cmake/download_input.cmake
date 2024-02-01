8a28a6506da2c7fdba7ebe7acc1708527018cb74
scivision <scivision@users.noreply.github.com>
2024-02-01 11:37:32 -0500

    remove timeouts

--- cmake/download_input.cmake
+++ cmake/download_input.cmake
@@ -46,7 +46,6 @@ if(NOT hash_ok)
   message(STATUS "${name}:DOWNLOAD: ${url} => ${archive}   ${hash}")
   file(DOWNLOAD ${url} ${archive} SHOW_PROGRESS
   EXPECTED_HASH SHA256=${hash}
-  INACTIVITY_TIMEOUT 60
   STATUS ret
   LOG log
   TLS_VERIFY ON
